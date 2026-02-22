07808d2873492a796f56f87480cb27a9ecfb6aed
Sarztak <sar3142168@gmail.com>
2026-02-22 08:23:54 -0600

    added linebreaks

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- enable line breaks so that word wrap whole rather than breaking mid-word
+vim.opt.linebreak = true
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -754,25 +757,25 @@ require('lazy').setup({
       keymap = {
         -- 'default' (recommended) for mappings similar to built-in completions
         --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
+        --    this will auto-import if your lsp supports it.
+        --    this will expand snippets if the lsp sent a snippet.
         -- 'super-tab' for tab to accept
         -- 'enter' for enter to accept
         -- 'none' for no mappings
         --
-        -- For an understanding of why the 'default' preset is recommended,
+        -- for an understanding of why the 'default' preset is recommended,
         -- you will need to read `:help ins-completion`
         --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        -- no, but seriously. please read `:help ins-completion`, it is really good!
         --
-        -- All presets have the following mappings:
+        -- all presets have the following mappings:
         -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
+        -- <c-space>: open menu or open docs if already open
+        -- <c-n>/<c-p> or <up>/<down>: select next/previous item
+        -- <c-e>: hide menu
+        -- <c-k>: toggle signature help
         --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
+        -- see :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
